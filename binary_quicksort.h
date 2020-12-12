841e44ea2b1db4572e6d2f755f00923029160d1f
MarcelPiNacy <marcelpi97@gmail.com>
2020-12-12 17:51:54 +0100

    Added overload that skips unnecessary O(N) passes.

--- binary_quicksort.h
+++ binary_quicksort.h
@@ -24,6 +24,7 @@
 
 #pragma once
 #include <iterator>
+#include <utility>
 
 namespace detail::binary_quicksort
 {
@@ -43,7 +44,7 @@ namespace detail::binary_quicksort
 	}
 
 	template <typename I, typename F>
-	constexpr void binary_quicksort_core(I begin, I end, size_t current_bit, F& extract_bit)
+	constexpr void binary_quicksort_core(I begin, I end, size_t current_bit, size_t min_bit, F& extract_bit)
 	{
 		using K = typename std::iterator_traits<I>::difference_type;
 
@@ -53,37 +54,56 @@ namespace detail::binary_quicksort
 			K left_size = 0;
 			K right_size = 0;
 
-			while (true)
+			do
 			{
 				middle = binary_partition(begin, end, current_bit, extract_bit);
-				if (current_bit == 0)
+				if (current_bit == min_bit)
 					return;
 				--current_bit;
 				left_size = std::distance(begin, middle);
 				right_size = std::distance(middle, end);
-				const bool is_right_tiny = right_size < 2;
-				if (!left_size < 2 || is_right_tiny)
-					break;
-				begin = is_right_tiny ? begin : middle;
-				end = is_right_tiny ? middle : end;
-			}
+			} while (left_size < 2 && right_size < 2);
 
 			if (left_size <= right_size)
 			{
-				binary_quicksort_core(begin, middle, current_bit, extract_bit);
+				binary_quicksort_core(begin, middle, current_bit, min_bit, extract_bit);
 				begin = middle;
 			}
 			else
 			{
-				binary_quicksort_core(middle, end, current_bit, extract_bit);
+				binary_quicksort_core(middle, end, current_bit, min_bit, extract_bit);
 				end = middle;
 			}
 		}
+	}
+
+	template <typename I, typename F, typename FFS>
+	constexpr void entry_point_min_max(I begin, I end, F& extract_bit, FFS&& find_first_set)
+	{
+		I min = begin;
+		I max = begin;
+		for (I i = begin; i < end; ++i)
+		{
+			if (*i < *min)
+				min = i;
+			if (*i > *max)
+				max = i;
+		}
+
+		const auto first = find_first_set(*max);
+		const auto last = find_first_set(*min);
+		binary_quicksort_core(begin, end, last, first, extract_bit);
 	}
 }
 
 template <typename I, typename F>
 constexpr void binary_quicksort(I begin, I end, size_t last_bit, F&& extract_bit)
 {
-	detail::binary_quicksort::binary_quicksort_core(begin, end, last_bit, extract_bit);
+	detail::binary_quicksort::binary_quicksort_core(begin, end, last_bit, 0, std::forward<F>(extract_bit));
+}
+
+template <typename I, typename F, typename FFS>
+constexpr void binary_quicksort(I begin, I end, F&& extract_bit, FFS&& find_first_set)
+{
+	detail::binary_quicksort::entry_point_min_max(begin, end, std::forward<F>(extract_bit), std::forward<FFS>(find_first_set));
 }
